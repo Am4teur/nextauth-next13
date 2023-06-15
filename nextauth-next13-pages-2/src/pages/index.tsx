148b6a9d74899730ac6550c94455936ebc8ef9db
Daniel Castro <dcastro@tripadvisor.com>
2023-06-15 07:32:51 +0100

    s5
    Add a signIn and signOut button to the Home page

--- nextauth-next13-pages-2/src/pages/index.tsx
+++ nextauth-next13-pages-2/src/pages/index.tsx
@@ -1,7 +1,32 @@
-import { Inter } from "next/font/google";
+import { Button, Flex, Heading, Text } from "@chakra-ui/react";
+import type { NextPage } from "next";
+import { signIn, signOut, useSession } from "next-auth/react";
 
-const inter = Inter({ subsets: ["latin"] });
+const Home: NextPage = () => {
+  const { data: session } = useSession();
 
-export default function Home() {
-  return <div>NextAuth demo</div>;
-}
+  return (
+    <Flex direction="column" alignItems="center" justifyContent="center">
+      <Heading>Home</Heading>
+      {session ? (
+        <>
+          <Text>
+            Signed in as <br />
+            <pre>{JSON.stringify(session, null, 2)}</pre>
+            <br />
+          </Text>
+          <Button onClick={() => signOut()}>Sign out</Button>
+        </>
+      ) : (
+        <>
+          <Flex direction="column" alignItems="center" justifyContent="center">
+            Not signed in <br />
+            <Button onClick={() => signIn()}>Sign in</Button>
+          </Flex>
+        </>
+      )}
+    </Flex>
+  );
+};
+
+export default Home;
