9d6a918c10f3d7774d6108415103e1a34d6562d9
Daniel Castro <dcastro@tripadvisor.com>
2023-06-16 07:41:40 +0100

    ms1

--- nextauth-next13-pages-2/src/pages/_app.tsx
+++ nextauth-next13-pages-2/src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
+import Navbar from "../components/Navbar";
 
 export default function App({
   Component,
@@ -9,6 +10,7 @@ export default function App({
   return (
     <ChakraProvider>
       <SessionProvider session={session}>
+        <Navbar />
         <Component {...pageProps} />
       </SessionProvider>
     </ChakraProvider>
